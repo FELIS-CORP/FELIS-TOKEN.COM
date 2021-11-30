d1cc0178cd638ba57d96f0239956bde2bdb2b022
FELIS <35664173+FELIS-CORP@users.noreply.github.com>
2021-11-29 18:24:06 -0600

    Update FELISBOT.ps1

--- FELISBOT.ps1
+++ FELISBOT.ps1
@@ -1,5 +1,5 @@
 #First we ping the utxo content of the FELIS wallet input and output it to a file.
-Write-Host 'Now listening to FELIS wallet UTXOs input... awaiting new transactions...' -ForegroundColor YELLOW
+Write-Host 'Now checking FELIS wallet UTXOs for new incoming transactions...' -ForegroundColor YELLOW
 cardano-cli query utxo --mainnet --address addr1qx3l3vmpt6sln6an5myyfwt29yrgu8aupy4cgrpw5vt2t90644467xe7zgx9khlgjmujuw0pehf62c0un6nhlfn05cas2z4y6a --out-file d:\website\FELISBOT\FELISUTXO.txt 
 #Rest to make sure the file is written before we check it
 Start-Sleep -Seconds 2
@@ -16,6 +16,6 @@ If (Select-String -Path d:\website\FELISBOT\FELISUTXO.txt -Pattern '2123400' -Si
 
 }
 
-Write-Host 'Checking again in 5 minutes...' -ForegroundColor BLUE
+Write-Host 'Checking again in 5 minutes' -ForegroundColor BLUE
 Start-Sleep -Seconds 268
 .\FELISBOT.ps1
